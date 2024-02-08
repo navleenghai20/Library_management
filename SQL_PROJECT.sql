56571129d43ca9dd986f1b9d7d00bca6252cdeef
navleenghai20 <navleenghai20@gmail.com>
2024-02-07 23:16:16 -0500

    Update SQL_PROJECT.sql

--- SQL_PROJECT.sql
+++ SQL_PROJECT.sql
@@ -1,39 +1,36 @@
 --Here we are creating a table to store the information about the books-----
 
-CREATE TABLE IF NOT EXISTS book_infor (
+CREATE TABLE IF NOT EXISTS book_inform (
 	bookid INT PRIMARY KEY NOT NULL,
 	book_title VARCHAR(100) NOT NULL,
-	author_name VARCHAR(50) NOT NULL,
-	category varchar(80) NOT NULL,
 	price INT NOT NULL,
 	copies INT NOT NULL
 );
 
 --Here we are inserting values to the table book_infor-----
 
-INSERT INTO book_infor (bookid, book_title, author_name, category, price, copies)
-VALUES
-  ('46567', 'X-Men: God Loves, Man Kills', 'Chris', 'Comics', 95, 70),
-  ('73563', 'Mike Tyson : Undisputed Truth', 'Larry Sloman, Mike Tyson', 'Sports', 700, 80),
-  ('86354', 'V for Vendetta', 'Alan Moore', 'Comics', 800, 40),
-  ('28476', 'When Breath Becomes Air', 'Paul Kalanithi', 'Medical', 650, 88),
-  ('99764', 'The Great Gatsby', 'F. Scott Fitzgerald', 'Fiction', 888, 150);
+INSERT INTO book_inform (bookid, book_title, price, copies) VALUES
 
+(4162768, 'Harry Potter', 150, 120 ), 
+
+(6743930, 'Rich Dad Poor Dad', 100, 80),
+
+(653637, 'King: A Life', 180, 98);
 
 --Here we are creating a table to store the information about the authors-----
 
 CREATE TABLE IF NOT EXISTS authors (
 	AuthorID INT PRIMARY KEY NOT NULL,	
-	BookID INT NOT NULL,
+	bookid INT NOT NULL,
 	AuthorName VARCHAR(50) NOT NULL,
 		CONSTRAINT fk_Bookid
-    		FOREIGN KEY(BookID) 
-        		REFERENCES book_info(BookID)
+    		FOREIGN KEY(bookid) 
+        		REFERENCES book_inform(bookid)
 );
 
 --Here we are inserting values to the table authors-----
 
-INSERT INTO authors (AuthorID, BookID, AuthorName) VALUES
+INSERT INTO authors (AuthorID, bookid, AuthorName) VALUES
 
 (12345, 4162768, 'J. K. Rowling'), 
 
@@ -47,17 +44,17 @@ INSERT INTO authors (AuthorID, BookID, AuthorName) VALUES
 
 CREATE TABLE IF NOT EXISTS book_copies (
 	CopiesID INT PRIMARY KEY NOT NULL, 
-	BookID INT NOT NULL,
+	bookid INT NOT NULL,
 	BranchID INT NOT NULL,
 	no_of_copies INT NOT NULL,
 		CONSTRAINT fk_Bookid
-    		FOREIGN KEY(BookID) 
-        		REFERENCES book_info(BookID)
+    		FOREIGN KEY(bookid) 
+        		REFERENCES book_inform(bookid)
 );
 
 --Here we are inserting values to the table book_copies-----
 
-INSERT INTO book_copies (CopiesID, BookID, BranchID, no_of_copies) VALUES
+INSERT INTO book_copies (CopiesID, bookid, BranchID, no_of_copies) VALUES
 
 (86, 4162768, 115, 1000), 
 
@@ -72,14 +69,17 @@ INSERT INTO book_copies (CopiesID, BookID, BranchID, no_of_copies) VALUES
 CREATE TABLE IF NOT EXISTS book_issue (
 	issue_id INT NOT NULL,
 	member_id varchar(20) NOT NULL,
-	BookID INT NOT NULL,
+	bookid INT NOT NULL,
 	due_date date NOT NULL,
-	last_reminded date DEFAULT NULL
+	last_reminded date DEFAULT NULL,
+	CONSTRAINT fk_Bookid
+    		FOREIGN KEY(bookid) 
+        		REFERENCES book_inform(bookid)
 ) ;
 
 --Here we are inserting values to the table book_issue-----
 
-INSERT INTO book_issue (issue_id, member_id, BookID, due_date, last_reminded) VALUES
+INSERT INTO book_issue (issue_id, member_id, bookid, due_date, last_reminded) VALUES
 
 (1001,67, 4162768, '2024-03-30', '2024-02-01'), 
 
@@ -88,11 +88,6 @@ INSERT INTO book_issue (issue_id, member_id, BookID, due_date, last_reminded) VA
 (1003, 876,  653637, '2024-03-22', '2024-01-17' )
 
 ;
-
-
-
-
-
 
 
 
